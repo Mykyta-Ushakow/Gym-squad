adccabab447814469b6af909fd0a71943b650f18
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2023-10-22 01:45:41 +0300

    comit1

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,6 +1,25 @@
-import * as main from "../main";
+import * as main from '../main';
+import axios from '../main';
+document
+  .querySelector('subscriptionForm')
+  .addEventListener('click', function (event) {
+    event.preventDefault();
 
-// тестова функція для перевірки роботи реекспорту, можеш видаляти
-export function logStuff() {
-    console.log("hi");
-}
+    const emailInput = document.querySelector('email');
+    const email = emailInput.value;
+
+    if (!emailInput.checkValidity()) {
+      alert('Будь ласка, введіть коректну електронну пошту.');
+    } else {
+      axios
+        .post('https://your-energy.b.goit.study/api/subscription', {
+          email: email,
+        })
+        .then(function (response) {
+          alert('Ваш запит успішно відправлено на сервер.');
+        })
+        .catch(function (error) {
+          alert('Помилка відправки запиту на сервер.');
+        });
+    }
+  });
