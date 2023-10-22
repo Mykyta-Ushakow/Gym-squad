137ce24079bc8c3125e812e4856741d690575e3f
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2023-10-22 21:57:22 +0300

    footer/JS

--- src/js/footer.js
+++ src/js/footer.js
@@ -2,24 +2,30 @@ import * as main from '../main';
 
 document
   .querySelector('#subscriptionForm')
-  .addEventListener('click', function (event) {
+  .addEventListener('submit', function (event) {
     event.preventDefault();
 
     const emailInput = document.querySelector('#footer-email');
     const email = emailInput.value;
-    
-    if (!emailInput.checkValidity()) {
-      alert('Будь ласка, введіть коректну електронну пошту.');
+
+    if (!checkValidity(email)) {
+      alert('Будь ласка, введіть коректну електронну пошту');
     } else {
       main.axios
         .post('https://your-energy.b.goit.study/api/subscription', {
           email: email,
         })
         .then(function (response) {
-          alert('Ваш запит успішно відправлено на сервер.');
+          alert('Ваш запит успішно відправлено');
+          emailInput.value = '';
         })
         .catch(function (error) {
-          alert('Помилка відправки запиту на сервер.');
+          alert('Помилка відправки запиту');
+          emailInput.value = '';
         });
     }
   });
+function checkValidity(email) {
+  const emailRegex = /[a-zA-Z0-9._\%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/;
+  return emailRegex.test(email);
+}
