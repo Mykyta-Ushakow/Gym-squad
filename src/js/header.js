44846fdee7d1dc4927011c70322ad8f7f8a65f81
cloudlight7 <cloudlight7@gmail.com>
2023-10-23 08:36:16 +0300

    header now

--- src/js/header.js
+++ src/js/header.js
@@ -1,28 +1,46 @@
 import * as main from "../main";
-const menuBtn = document.querySelector('[data-menu-button]');
+const menuBtn = document.querySelector('[data-menu-open]');
 const menuBtnCl = document.querySelector('[data-menu-close]');
 const menu = document.querySelector('.nav');
 const homePageLink = document.querySelector('[data-action="honepage"]');
-const favoritePageLink = document.querySelector('[data-action="favoritespage"]');
+const favoritePageLink = document.querySelector('[data-action="favoritespage"]'); 
+const menuBackdrope = document.querySelector('.menuBackdrope'); 
 const body = document.body;
-if(document.location.pathname ==="/Gym-squad/index.html"|| document.location.pathname ==="/Gym-squad"){
+
+
+if(document.location.pathname ==="/Gym-squad/index.html"|| document.location.pathname ==="/Gym-squad/"){
     homePageLink.classList.toggle('current');
 }
 if(document.location.pathname ==="/Gym-squad/favorite.html"){
     favoritePageLink.classList.toggle('current');
 }
 
+document.addEventListener("keydown", event => {
+    if (event.key === 'Escape') {
+        closMenu();
+    }
+});
+menuBtnCl.addEventListener('click', closMenu);
+
 menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    menuBtn.classList.toggle('active');
-    menuBtnCl.classList.toggle('active');
-    body.classList.toggle('lock');
+    menu.classList.add('active');
+    menuBtn.classList.add('is-hidden');
+    menuBtnCl.classList.remove('is-hidden');
+    body.classList.add('lock');
+    menuBackdrope.classList.add('active');
 
-    })
-menuBtnCl.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    menuBtn.classList.toggle('active');
-    menuBtnCl.classList.toggle('active');
-    body.classList.toggle('lock');
-
-    })
+})
+function closMenu() {
+    menu.classList.remove('active');
+    menuBtn.classList.remove('is-hidden');
+    menuBtnCl.classList.add('is-hidden');
+    body.classList.remove('lock');
+    menuBackdrope.classList.remove('active');
+}
+menuBackdrope.addEventListener("click", funk);
+    
+function funk(ev) {
+        if (ev.target.classList[0] === 'menuBackdrope') {
+        closMenu();
+    }
+}
